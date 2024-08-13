22b1958c4d44dd4e9a426035bcab60d966f7ff56
YashasPatil <yashaspatil@gmail.com>
2024-08-12 19:35:12 -0700

    built all the logic for the calculator

--- calc.js
+++ calc.js
@@ -1,3 +1,14 @@
+// Variables involved in the operation
+let firstNum;
+let signValue;
+let secondNum;
+let answer;
+
+//Variable that will determine if an operation is currently going on
+let clicked = false;
+
+
+//Functions for addition, subtraction, divison, and multiplication
 function add (number1,number2){
     return number1 + number2;
 }
@@ -14,38 +25,70 @@ function multiply (number1, number2) {
 function subtract (number1,number2) {
     return number1 - number2;
 }
-let firstNum =30;
-let sign;
-let secondNum;
-function operate (sign, firstNum, secondNum) {
-    if (sign === "division") {
+function operate (signValue, firstNum, secondNum) {
+    if (signValue === "/") {
        return divide (firstNum, secondNum);
-    } else if (sign === "multiplication") {
+    } else if (signValue === "*") {
         return multiply (firstNum, secondNum);
-    } else if (sign === "addition") {
+    } else if (signValue === "+") {
        return add (firstNum, secondNum);
-    } else if (sign === "subtraction") {
+    } else if (signValue === "-") {
       return  subtract (firstNum, secondNum);
     }
 }
+
+//Important body elements
 const display = document.getElementById ('display');
-const buttons = document.querySelectorAll('.content');
-const clear   = document.getElementById('clear');
-buttons.forEach(button => {
+const buttons = document.querySelectorAll('.displayed');
+const numbers = document.querySelectorAll('.content');
+const signs = document.querySelectorAll('.operator');
+const clear = document.getElementById('clear');
+const equal = document.getElementById('equal');
+
+//Functions to display numbers and operators
+numbers.forEach((button) => {
     button.addEventListener('click', () => {
-        const buttonValue = button.getAttribute('data-value');
-        // Get the value from the button's data attribute
-        // Update the display with the button's value
-        if (display.textContent === '0') {
-            display.textContent = buttonValue;
+      const buttonValue = button.getAttribute('data-value'); // Get the value from the button's data attribute
+      // Update the display with the button's value
+      if (display.textContent === '0') {
+        display.textContent = buttonValue;
+      } else {
+        display.textContent += buttonValue;
+      }
+    });
+  });
+signs.forEach((sign) =>{
+    sign.addEventListener('click', () => {
+        signValue = sign.getAttribute('data-value');
+        if (clicked === true ) {
+            return false;
         } else {
-            display.textContent += buttonValue;
+            display.textContent += signValue;
+            clicked = true;
         }
     });
 });
+
+//Function to reset everything
 clear.addEventListener('click', () => {
     display.textContent = '0';
+    signValue;
     firstNum;
     secondNum;
-    sign;
-})
+    clicked = false;
+});
+
+equal.addEventListener('click', () =>{
+    const input = display.textContent;
+    const part = input.split(signValue);
+    let displayed;
+    //Make sure there's a valid operator
+    if (clicked===true){
+        firstNum = parseInt(part[0]);  //Needed so that the addition function doesn't join the two strings
+        secondNum = parseInt (part[1]);
+        answer = operate(signValue, firstNum, secondNum);
+        displayed = Math.round(answer*1000)/1000;
+        display.textContent = displayed;
+        clicked = false;
+    } 
+});
